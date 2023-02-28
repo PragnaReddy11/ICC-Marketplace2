472aa6367d871c4768c315caf1a6ee8722c51186
Machupalli Sree Pragna <88078068+PragnaReddy11@users.noreply.github.com>
2023-03-01 00:48:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Collections from "./Components/Collections";
+import Bidders from "./Components/Bidders";
+import Community from "./Components/Community";
+import Footer from "./Components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  });
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Collections />
+      <Bidders />
+      <Community />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data/item-nft.js
@@ -0,0 +1,118 @@
+import image2 from "../Components/img/image2.jpg"
+
+
+const data =[
+  {
+    "id": 1,
+    "image": '../components/img/kohli.jpg',
+    "title": "Virat Kohli",
+    "username": "India",
+    "price": 5🪙
+  },
+  {
+    "id": 2,
+    "image":'../components/img/rohit.jpg' ,
+    "title": "Rohit Sharma",
+    "username": "India",
+    "price": 5🪙
+  },
+  {
+    "id": 3,
+    "image": '../components/img/smriti.jpg',
+    "title": "Smriti Mandana",
+    "username": "India",
+    "price": 5🪙
+  },
+  {
+    "id": 4,
+    "image": '../components/img/surya.jpg',
+    "title": "Surya Kumar Yadav",
+    "username": "India",
+    "price": 5🪙
+  },
+  {
+    "id": 5,
+    "image": '../components/img/hasranga.jpg',
+    "title": "Wanindu Haranga",
+    "username": "SriLanka",
+    "price": 3🪙
+  },
+  {
+    "id": 6,
+    "image": '../components/img/Shakib.jpg',
+    "title": "Shaikib Al Hasan",
+    "username": "Bangaldesh",
+    "price": 3🪙
+  },
+  {
+    "id": 7,
+    "image": '../components/img/alyssa.jpg',
+    "title": "Alyssa Healy",
+    "username": "Australia",
+    "price": 3🪙
+  },
+  {
+    "id": 8,
+    "image": '../components/img/gill.jpg',
+    "title": "Shubman Gill",
+    "username": "India",
+    "price": 3🪙
+  },
+  {
+    "id": 8,
+    "image": '../components/img/jadeja.jpg',
+    "title": "Ravindra Jadeja ",
+    "username": "India",
+    "price": 3🪙
+  },
+  {
+    "id": 9,
+    "image": '../components/img/shefali.jpg',
+    "title": "Shefali Verma",
+    "username": "India",
+    "price": 3🪙
+  },
+  {
+    "id": 9,
+    "image": '../components/img/warner.jpg',
+    "title": "David Warner",
+    "username": "Australia",
+    "price": 3🪙
+  },
+  {
+    "id": 10,
+    "image": '../components/img/joss.jpg',
+    "title": "Joss Buttle",
+    "username": "England",
+    "price": 3🪙
+  },
+  {
+    "id": 10,
+    "image": '../components/img/boult.jpg',
+    "title": "Trent Boult",
+    "username": "England",
+    "price": 3🪙
+  },
+  {
+    "id": 10,
+    "image": '../components/img/joss.jpg',
+    "title": "Joss Buttler",
+    "username": "England",
+    "price": 3🪙
+  },
+  {
+    "id": 10,
+    "image": '../components/img/marnus.jpg',
+    "title": 'Marnus Labsuchagne",
+    "username": "Australia",
+    "price": 3🪙
+  },
+  {
+    "id": 10,
+    "image": '../components/img/hayley.jpg',
+    "title": "Heyley Mathews",
+    "username": "West Indies",
+    "price": 3🪙
+  }
+]
+export default data
